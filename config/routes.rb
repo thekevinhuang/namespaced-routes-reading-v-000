137e0526d5ccabc90af2453769da8dfef372e951
Kevin Huang <yellow.khaos@gmail.com>
2018-07-06 02:55:59 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
+  get '/admin/stats', to: 'stats#index'
+  get '/admin/authors/new', to: 'authors#new'
+  get '/admin/authors/delete', to: 'authors#delete'
+  get '/admin/authors/create', to: 'authors#create'
+  get '/admin/comments/moderate', to: 'comments#moderate'
+  
+  
   root 'posts#index'
 end
